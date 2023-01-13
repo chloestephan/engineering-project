b609955df1354123acf57e09f75a35c9bcb0f793
Tristan-F <tristan.fievet@gmail.com>
2023-01-13 15:09:55 +0100

    login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import Register from "./Register";
+import Login from "./Login";
+// import Register from "./Register";
 
 function App() {
   return (
     <main className="App">
-      <Register />
+      <Login />
     </main>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
+const express = require("express");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 const app = express();
 const port = 5000;
-const cors = require('cors');
-const {Client} = require("pg");
+const cors = require("cors");
+const { Client } = require("pg");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require('dotenv').config()
 
 const client = new Client({
-  password : "root",
-  user : "root",
-  host : "postgres"
+  password: "root",
+  user: "root",
+  host: "postgres",
 });
 
 app.use(cors());
 
-app.post('/register', jsonParser,(req, res) => {
+// Register a user
+app.post("/register", jsonParser, (req, res) => {
   // TODO Check if email already exists
   // If exists
   // res.status(409).send('User already exists');
@@ -24,7 +28,47 @@ app.post('/register', jsonParser,(req, res) => {
   // TODO Hash password
   // TODO Store user in database
   // If success
-  res.status(200).send('User registered');
+  res.status(200).send("User registered");
 });
 
-app.listen(port, () => console.log('Server running on port 5000'));
+// Login a user
+app.post("/login", jsonParser, (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send("Missing information");
+  }
+
+  // TODO Check if user exists with email
+  // If not exists or wrong password
+  // res.status(404).send('Wrong information');
+  // If exists check bcrypt password
+  // If wrong password
+  // res.status(404).send('Wrong information');
+  // If success
+  const accessToken = jwt.sign(
+    {
+      email: email,
+      // TODO add more information from database (company, username, etc.)
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+  const refreshToken = jwt.sign(
+    {
+      email: email,
+      // TODO add more information from database (company, username, etc.)
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "24h",
+    }
+  );
+  // TODO Store refresh token in database
+  
+  res.cookie("jwt", refreshToken, { httpOnly: true, secure: true, sameSite: "none" });
+  res.status(200).send({ message: "User logged in", accessToken: accessToken });
+});
+
+app.listen(port, () => console.log("Server running on port 5000"));
