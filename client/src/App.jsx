105624944533329216afd2a43ed4e80842103969
Chloé Stéphan <chloe.stephan@efrei.net>
2023-01-20 10:41:03 +0100

    parce que je sais pas copiter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import Register from "./components/main/register/Register";
+import RegisterForm from "./components/main/register/RegisterForm";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import LoginForm from "./components/main/login/LoginForm";
