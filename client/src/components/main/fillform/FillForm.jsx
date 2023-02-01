f8a5f029e2b12090ce06f9f1277f6f8ce02e0053
Nastir-F <tristan94000@gmail.com>
2023-02-01 14:20:11 +0100

    change how handle check customer and link to form

--- client/src/components/main/fillform/FillForm.jsx
+++ client/src/components/main/fillform/FillForm.jsx
@@ -1,9 +1,3 @@
-import { useEffect, useState } from "react";
-import useAuth from "../../../hooks/useAuth";
-import axios from "../../../api/axios";
-import { useLocation } from "react-router-dom";
-
-const CHECK_CUSTOMER_LINK_URL = "/check-customer-link";
 const FillForm = () => {
   return (
     <section>
--- server/routes/customer/checkCustomerLink.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const checkCustomerLink = require("../../controllers/customer/checkCustomerLinkController.js");
-
-router.post("/", checkCustomerLink.handleCheckCustomerLink);
-
-module.exports = router;
