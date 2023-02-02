7d91983fc6f630c596dde0bf161fca54c40250d1
NicolasCrssx <nicolas.cresseaux@gmail.com>
2023-02-02 16:41:38 +0100

    (le dernier commit a pas bien marché jsp pourquoi)

--- client/src/components/main/forgotPassword/ForgotPasswordForm.jsx
+++ client/src/components/main/forgotPassword/ForgotPasswordForm.jsx
@@ -4,6 +4,7 @@ import ErrorMessageForm from "../../utils/MessageForm/ErrorMessageForm";
 import SuccessMessageForm from "../../utils/MessageForm/SuccessMessageForm";
 import DefaultInputContainer from "../../utils/DefaultInput/DefaultInputContainer";
 import axios from "../../../api/axios";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 const FORGOT_PASSWORD_URL = "/forgot-password";
