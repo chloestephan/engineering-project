ea0f506bba30da1641c52ff7a4f4e0b3b71ba49c
Nastir-F <tristan94000@gmail.com>
2023-03-01 12:10:05 +0100

    Fillform with json

--- client/src/components/main/fillform/FillForm.jsx
+++ client/src/components/main/fillform/FillForm.jsx
@@ -1,53 +1,42 @@
-import { useEffect, useState } from "react";
-import { FormContext } from "../../utils/FormBuilderInput/FormBuilderContext";
+import { useState } from "react";
 import formJSON from "../../../data/form.json";
-import FormBuilderInputContainer from "../../utils/FormBuilderInput/FormBuilderInputContainer";
 
 const FillForm = () => {
-  const [fields, setFields] = useState(null);
-  useEffect(() => {
-    setFields(formJSON);
-  }, []);
+  const [currentQuestion, setCurrentQuestion] = useState(formJSON[0]);
+  const [endPoint, setEndPoint] = useState("");
+  const [lastQuestion] = useState([]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // TODO CHANGE VALUE FOR MULTIPLE SELECT WHEN SUBMIT AND ADD ALL THE SELECTED OPTIONS
-    console.log(fields);
+  const handleAnswerQuestion = (option) => {
+    lastQuestion.push(currentQuestion);
+    if (option.next === "") {
+      setEndPoint(option.end);
+      setCurrentQuestion(null);
+    } else {
+      setEndPoint("");
+      setCurrentQuestion(formJSON.find((question) => question.id === option.next));
+    }
   };
 
-  const handleChange = (id, event) => {
-    const newFields = [...fields];
-    const fieldsToUpdate = newFields.find((field) => field.id === id);
-    if (fieldsToUpdate.type === "checkbox" || fieldsToUpdate.type === "radio") {
-      fieldsToUpdate.value = event.target.checked;
-    } else {
-      fieldsToUpdate.value = event.target.value;
-    }
-    setFields(newFields);
+  const handlePreviousQuestion = () => {
+    setCurrentQuestion(lastQuestion.pop());
+    setEndPoint("");
   };
 
   return (
     <section>
-      <h1>Bienvenue !</h1>
-      <FormContext.Provider value={{ handleChange }}>
-        {formJSON.map((item) => (
-          <div key={item.id}>
-            <FormBuilderInputContainer
-              inputId={item.id}
-              inputLabel={item.label}
-              inputType={item.type}
-              isInputRequired={item.required ?? true}
-              inputPlaceholder={item.placeholder ?? ""}
-              inputOptions={item.options ?? []}
-              inputValue={item.value}
-              isMultiple={item.multiple ?? false}
-            />
-          </div>
-        ))}
-        <button type="submit" onClick={handleSubmit}>
-          Submit
+      <h1>Bienvenue dans le formulaire !</h1>
+      <br />
+      <h3>{currentQuestion?.title}</h3>
+      <br />
+      {currentQuestion?.options.map((option) => (
+        <button key={option.id} onClick={() => handleAnswerQuestion(option)}>
+          {option.title}
         </button>
-      </FormContext.Provider>
+      ))}
+      {endPoint && <h3>{endPoint}</h3>}
+      {lastQuestion.length !== 0 && (
+        <button onClick={() => handlePreviousQuestion()}>Question précédente</button>
+      )}
     </section>
   );
 };
--- client/src/components/utils/FormBuilderInput/FormBuilderInputLabel.jsx
+++
@@ -1,9 +0,0 @@
-const FormBuilderInputLabel = ({ inputId, inputLabel, isInputRequired }) => {
-  return (
-    <label htmlFor={"input-" + inputId} className="form-label">
-      {inputLabel} {isInputRequired && <span className="text-danger">*</span>}
-    </label>
-  );
-};
-
-export default FormBuilderInputLabel;
