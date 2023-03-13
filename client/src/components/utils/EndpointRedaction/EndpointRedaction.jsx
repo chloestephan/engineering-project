291c130d797750ee34596aa2ca0280359a907932
Nastir-F <tristan94000@gmail.com>
2023-03-13 12:25:28 +0100

    display explanation for endpoints

---
+++ client/src/components/utils/EndpointRedaction/EndpointRedaction.jsx
@@ -0,0 +1,21 @@
+import endpoint from "../../../data/endpoint.json";
+
+const EndpointRedaction = ({ endpointTitle }) => {
+  return (
+    <>
+      <h2>{endpointTitle}</h2>
+      <br />
+      {endpoint
+        .filter((endpoint) => endpoint.title === endpointTitle)
+        .map((endpoint) =>
+          endpoint.redaction ? (
+            <p>{endpoint.redaction}</p>
+          ) : (
+            <p>Il n'y a pas d'explication pour cette solution</p>
+          )
+        )}
+    </>
+  );
+};
+
+export default EndpointRedaction;
