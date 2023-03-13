c174321f60bace60c668e527d5a958bdc81195b7
NicolasCrssx <nicolas.cresseaux@gmail.com>
2023-03-13 17:35:51 +0100

    Initialisation de l'utilisation du token

--- client/src/components/requireauth/RequireAuthAdmin.jsx
+++ client/src/components/requireauth/RequireAuthAdmin.jsx
@@ -1,11 +1,26 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { accountService } from "../../services/account.service";
 import useAuth from "../../hooks/useAuth";
 
 const RequireAuthAdmin = () => {
-  const { auth } = useAuth();
+
   const location = useLocation();
 
-  return auth?.email ? <Outlet /> : <Navigate to="/admin-login" state={{ from: location }} replace />;
+  if (!accountService.isLogged()){
+    return <Navigate to="/admin-login" state={{ from: location }} replace />
+  } else {
+    return <Outlet />
+  }
+
+  // const { auth } = useAuth();
+  // const location = useLocation();
+
+  // return (
+  //   auth?.email ? 
+  //     <Outlet /> 
+  //     : 
+  //     <Navigate to="/admin-login" state={{ from: location }} replace />
+  // );
 };
 
 export default RequireAuthAdmin;
